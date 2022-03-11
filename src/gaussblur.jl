968960e2f7b5d6dfb3f4abfceee3e05ff007a4bd
jonat <jawhite@caltech.edu>
2022-03-11 13:45:09 -0800

    delete commented out code

--- src/gaussblur.jl
+++ src/gaussblur.jl
@@ -97,12 +97,8 @@ computeGradient(model :: GaussBlur2D, thetas :: Matrix{Float64}, v :: Vector{Flo
 function computeGradient(model :: GaussBlur2D, thetas :: Matrix{Float64}, r :: Vector{Float64})
 
   # v is the gradient of loss (vi = resid_i/norm(residuals))
-  #println("computing gradient...")
-  #println("size(thetas): ", size(thetas))
   R = reshape(r, model.n_pixels, model.n_pixels)
-  #Rt = R'
 
-  #gradient = zeros(size(thetas)[1] + 1, size(thetas)[2])
   gradient = zeros(size(thetas))
   #allocate temporary variables...
   f_x1 = zeros(model.n_pixels)
@@ -118,11 +114,6 @@ function computeGradient(model :: GaussBlur2D, thetas :: Matrix{Float64}, r :: V
   for k = 1:size(thetas,2)
     point = vec(thetas[:,k])
     x₁ₖ, x₂ₖ, σₖ, wₖ = point
-    #computeFG!(model, point[1], point[3], f_x1, fpx1, fpx1s)
-    #computeFG!(model, point[2], point[3], f_x2, fpx2, fpx2s)
-    #∂loss∂x₁ₖ = - 2 * point[4] * (f_x2' * R * fpx1)/(point[3]^2)
-    #∂loss∂x₂ₖ = - 2 * point[4] * (fpx2' * R * f_x1)/(point[3]^2)
-    #∂loss∂σₖ = - 2 * point[4] * ( f_x2' * R * fpx1s + fpx2s' * R * f_x1)/(point[3]^3)
     computeFG!(model, x₁ₖ, σₖ, f_x1, fpx1, fpx1s)
     computeFG!(model, x₂ₖ, σₖ, f_x2, fpx2, fpx2s)
     ∂loss∂x₁ₖ = - 2 * wₖ * (f_x2' * R * fpx1)/(σₖ^2)
@@ -135,15 +126,13 @@ function computeGradient(model :: GaussBlur2D, thetas :: Matrix{Float64}, r :: V
 end
 
 ##not optimized.
-#function computeFs(x,n_pixels :: Int64, sigma :: Float64, psf_thresh :: Int64, result :: Matrix{Float64}  = zeros(n_pixels,length(x)))
 function computeFs(x,n_pixels :: Int64, sigma, psf_thresh :: Int64, result :: Matrix{Float64}  = zeros(n_pixels,length(x)))
   nx = length(x)
-  #sigma2_inv = 1.0/(2.0*sigma^2)
   @simd for k = 1:nx
     sigma2_inv = 1.0/(2.0*sigma[k]^2)
     @inbounds start = maximum([1, round(Int64, x[k])-psf_thresh])
     @inbounds stop = minimum([n_pixels, round(Int64, x[k])+psf_thresh])
-    @simd for i = start:stop#1:n_pixels
+    @simd for i = start:stop
       @inbounds result[i, k] = exp(-sigma2_inv*(i-x[k])^2)
     end
   end
@@ -156,17 +145,12 @@ function computeFG!(s :: GaussBlur2D, x :: Float64, sigma :: Float64, f :: Vecto
   start = maximum([1, round(Int64, x)-s.psf_thresh])
   stop = minimum([n_pixels, round(Int64, x)+s.psf_thresh])
   sigma2_inv = 1.0/(2*sigma^2)
-  #prefactor_g = -1.0/(sigma^2)
-  #prefactor_gs = 1.0/(sigma^3)
 
   #might wanna just bite the bullet and allocate here...
-  #@fastmath @inbounds @simd  for i = 1:n_pixels
   @fastmath @inbounds @simd for i = start:stop
     f[i] = exp(-sigma2_inv*(i-x)^2)
-    #g[i] = -prefactor_g*(i-x)*f[i]
     g[i] = (i-x)*f[i]
     gs[i] = g[i]*(i-x)
-    #g_sig[i] = -g_spat[i]*(i-x)/sigma #prefactor_gs*f[i](i-x)^2
   end
 end
 
@@ -177,22 +161,19 @@ function phi(s :: GaussBlur2D, parameters :: Matrix{Float64})
   end
   v_x = computeFs(vec(parameters[1,:]),n_pixels,parameters[3,:], s.psf_thresh)
   v_y = computeFs(vec(parameters[2,:]),n_pixels,parameters[3,:], s.psf_thresh)
-  #scale!(v_x, weights)
-  #v_x .= v_x * diagm(weights)
   v_x .= v_x * diagm(parameters[4,:])
 
   return vec(v_y*v_x')
 end
 
 
-function solveFiniteDimProblem(model :: GaussBlur, thetas, y, tau)#, fittype=LassoPath)
+function solveFiniteDimProblem(model :: GaussBlur, thetas, y, tau)
   nthetas = size(thetas)[2]
   nparams = size(thetas)[1]
   if nthetas == 0
     return Float64[]
   end
 
-  #A = zeros(nthetas, model.n_pixels^2)
   if nparams == 3
     A = zeros(model.n_pixels^2, nthetas)
   elseif nparams == 5
@@ -200,43 +181,11 @@ function solveFiniteDimProblem(model :: GaussBlur, thetas, y, tau)#, fittype=Las
   end
 
   for i = 1:nthetas
-    #A[i, :] = vec(phi(model, reshape(thetas[:,i], 2, 1), [1.0]))
     A[:, i] = vec(phi(model, reshape(thetas[:,i], nparams, 1)))
   end
 
-  #println("sum y: ", sum(y))
-  #println("nthetas: ", nthetas)
-  #results = fit(LassoPath, A, y)
-
-  #results = fit(fittype, A, y)
   results = lm(A, y)
   return coef(results)
-  """
-  if fittype != LassoPath
-    results = lm(A, y)
-    println(results)
-  else
-    results = fit(fittype, A, y)
-  end
-
-  #results = fit(GeneralizedLinearModel, A, y)
-  #println(results.coefs)
-  n_lambdas = size(results.coefs)[2]
-  coeffs = []
-  for i = 1:(n_lambdas)
-    sum_coeffs = sum(results.coefs[:, n_lambdas + 1 - i])
-    #println("sum_coeffs ", i , ": ", sum_coeffs)
-    if sum(results.coefs[:, n_lambdas + 1 - i]) <= tau
-      println("lambda: ", i)
-      println("sum(coefs): ", sum_coeffs)
-      coeffs = Array(results.coefs[:, n_lambdas + 1 - i])
-      break
-    end
-  end
-  return coeffs
-  #results = fit(LassoPath, A, y, λ = [5.0])
-  #return Array(results.coefs[:,1])
-  """
 end
 
 function lmo(model :: GaussBlur2D, r :: Vector{Float64})
@@ -247,56 +196,20 @@ function lmo(model :: GaussBlur2D, r :: Vector{Float64})
   ub1 = minimum([ub_0[1], initial_x[1] + 0.5])
   ub2 = minimum([ub_0[2], initial_x[2] + 0.5])
   lb = [lb1, lb2, initial_x[3], initial_x[4]]
-  #ub = [ub1, ub2, ub[3]]
   ub = [ub1, ub2, initial_x[3], initial_x[4]]
-  #println("Stating Point: ", initial_x)
   p = length(lb)
-  #println("lb: ", lb)
-  #println("ub: ", ub)
+
   optx, optf = localDescent(model, LSLoss(), reshape(initial_x, 4, 1), [1.0], r, (lb, ub))
 
   lb = [optx[1], optx[2], lb_0[3], 0.0]
   ub = [optx[1], optx[2], ub_0[3], initial_x[4]*10]
   optx, optf = localDescent(model, LSLoss(), optx, [1.0], r, (lb, ub))
-  #println("lmo optx: ", optx)
-"""
-  function f_and_g!(point :: Vector{Float64}, gradient_storage :: Vector{Float64})
-    #println("point: ", point)
-    output = phi(model,reshape(point,p,1), [1.0])
-    ip = dot(output,v)
-    s = sign(ip)
-    gradient_storage[:] = -s*computeGradient(model, [1.0],reshape(point,length(point),1), v)
-    #gradient_storage[:] = -s*computeGradient(model, [1.0],reshape(point,length(point),1), ip)
-    #println("gradient: ", gradient_storage)
-    return -s*ip
-  end
-
-
-  function f_and_g!(points :: Vector{Float64}, gradient_storage :: Vector{Float64})
-    println("f_and_g!")
-    points = reshape(points,model.n_pixels,1)
-    output = phi(model,points,[1.0])#s.w)
-    residual = output .+ r
-    l,v_star = loss(s.lossFn,residual)
-    gradient_storage[:] = computeGradient(model.sim, [1.0], points, v_star)
-    return l
-  end
-
-  opt = Opt(:LD_MMA, p)
-  initializeOptimizer!(model, opt)
-  min_objective!(opt, f_and_g!)
-  lower_bounds!(opt, lb)
-  upper_bounds!(opt, ub)
-  (optf,optx,ret) = optimize(opt, initial_x)
-  """
-  #println("optx: ", optx)
-  #println("ret: ", ret)
+ 
   return (optx,optf)
 end
 
 function localDescent_coord(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Float64}, w :: Vector{Float64}, y :: Vector{Float64})
   lb,ub = parameterBounds(s)
-  #lb, ub = bounds
   nPoints = size(thetas,2)
   im_lb = fill(lb[1], nPoints)
   im_ub = fill(ub[2], nPoints)
@@ -306,9 +219,7 @@ function localDescent_coord(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Fl
   x2_ub = broadcast((a, b) -> a < b ? a : b, im_ub, thetas[2,:] .+ s.psf_thresh/2)
 
   lb = vec(vcat(x1_lb', x2_lb'))
-  #println("lb: ", lb)
   ub = vec(vcat(x1_ub', x2_ub'))
-  #println("ub: ", ub)
 
   p = size(thetas,1)
   su = SupportUpdateProblem(nPoints,p,s,y,w,lossFn)
@@ -316,15 +227,12 @@ function localDescent_coord(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Fl
       ps = reshape(x, 2, Int64(length(x)/2))
       ps = vcat(ps, thetas[3:4,:])
       points = reshape(ps,su.p,su.nPoints)
-      #println("su.w: ", su.w)
-      output = phi(s,points)#s.w)
+      output = phi(s,points)
       residual = su.y .- output
-      #l,v_star = loss(s.lossFn,residual)
       l = sum(residual.^2)
       g[:] = computeGradient(s, points, residual)[1:2, :]
       l
   end
-  #println("nPoints: ", 2*nPoints)
   opt = Opt(NLopt.LD_MMA, 2*nPoints)
   initializeOptimizer!(s, opt)
   min_objective!(opt, f_and_g!)
@@ -339,7 +247,6 @@ end
 
 function localDescent_sigma(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Float64}, w :: Vector{Float64}, y :: Vector{Float64})#, bounds = parameterBounds(s))
   lb,ub = parameterBounds(s)
-  #lb, ub = bounds
   s_lb = lb[3]
   s_ub = ub[3]
   w_lb = 0
@@ -352,7 +259,6 @@ function localDescent_sigma(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Fl
   ub = vec(vcat(fill(s_ub, nPoints)', w_ub'))
   p = size(thetas,1)
   su = SupportUpdateProblem(nPoints,p,s,y,w,lossFn)
-  #f_and_g!(x,g) = localDescent_f_and_g!(x,g,su)
 
   #coordinates are fixed, sigma is input parameter to be optimized
   function f_and_g!(sigma_weight,g)
@@ -360,13 +266,10 @@ function localDescent_sigma(s :: GaussBlur2D, lossFn :: Loss, thetas ::Matrix{Fl
       sws = reshape(sigma_weight, 2, Int64(length(sigma_weight)/2))
       ps = vcat(crds, sws)
 
-      #results = localDescent_f_and_g!(vec(ps),g,su)
       points = reshape(ps,su.p,su.nPoints)
       output = phi(su.s,points)
       residual = su.y .- output
-      #l,v_star = loss(su.lossFn,residual)
       l = sum(residual.^2)
-      #grd = computeGradient(s, points[4,:], points, residual)
       g[:] = computeGradient(s, points, residual)[3:4, :]
       l
   end
