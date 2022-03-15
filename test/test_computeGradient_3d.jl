193737888b8b5cc2c248b421623785a3e948e1d4
Jonathan A. White <jawhite@caltech.edu>
2022-03-15 16:08:07 -0700

    working on 3d gradient test

--- test/test_computeGradient_3d.jl
+++ test/test_computeGradient_3d.jl
@@ -28,10 +28,10 @@ test_img = phi(gblur, p_true) #.+ rand(32,32,15)/1000#rand(1024)./1000
 
 #SparseInverseProblems.
 
-function resids(img, ps)#, ws)
-    trial_output = phi(gblur, ps)#, ws)
-    residuals = img .- trial_output #.- img
-    residuals#./norm(residuals)
+function resids(img, ps)
+    trial_output = phi(gblur, ps)
+    residuals = img .- trial_output
+    residuals
 end
 
 function trial_gradient(test_img, _trial_point)
@@ -42,24 +42,25 @@ function trial_gradient(test_img, _trial_point)
     return gradient
 end
 
-function empPartialDeriv(img, trial_point, trial_weight, Δ)
-    residuals = resids(img, trial_point .- Δ, trial_weight)
-    #normed_residuals = normed_resids(img, trial_point, trial_weight)
-    Δresiduals = resids(img, trial_point .+ Δ, trial_weight)
-    #sum(Δnormed_residuals .-normed_residuals)/(2*norm(Δ))
-    sum(Δresiduals .- residuals)/(2*norm(Δ))
+function empPartialDeriv(img, trial_point, Δ)
+    residuals = resids(img, trial_point .- Δ)
+    Δresiduals = resids(img, trial_point .+ Δ)
+    sum(Δresiduals.^2 .- residuals.^2)/(2*norm(Δ))
 end
 
-function empGrad(img, _trial_point, _trial_weight, Δ)
-    trial_point = Matrix{Float64}(undef, 5, 1)
+function empGrad(img, _trial_point, Δ)
+    trial_point = Matrix{Float64}(undef, 6, 1)
     trial_point[:] = _trial_point
-    ΔlossΔx1 = empPartialDeriv(img, trial_point, [_trial_weight], [ Δ, 0 ,0])
-    ΔlossΔx2 = empPartialDeriv(img, trial_point, [_trial_weight], [0,  Δ, 0])
-    ΔlossΔσ = empPartialDeriv(img, trial_point, [_trial_weight], [0, 0,  Δ])
-    [ΔlossΔx1, ΔlossΔx2, ΔlossΔσ]
+    ΔlossΔx₁ = empPartialDeriv(img, trial_point, [ Δ, 0 ,0, 0, 0, 0])
+    ΔlossΔx₂ = empPartialDeriv(img, trial_point, [0,  Δ, 0, 0, 0, 0])
+    ΔlossΔx₃ = empPartialDeriv(img, trial_point, [0, 0,  Δ, 0, 0, 0])
+    ΔlossΔσxy = empPartialDeriv(img, trial_point, [0, 0, 0, Δ, 0, 0])
+    ΔlossΔσz = empPartialDeriv(img, trial_point, [0, 0, 0, 0, Δ, 0])
+    ΔlossΔw = empPartialDeriv(img, trial_point, [0, 0, 0, 0, 0, Δ])
+    [ΔlossΔx₁, ΔlossΔx₂, ΔlossΔx₃, ΔlossΔσxy, ΔlossΔσz, ΔlossΔw]
 end
 
-Δ = 0.00001
+Δ = 0.01
 
 #println("gradient: ", trial_gradient(test_img, [16.0, 16.0, 7.0, 1.4, 1.1, 1.0]))
 #println("empGrad: ", empGrad(test_img, [16.0, 16.0, 1.0], 1.0, Δ))
@@ -72,22 +73,22 @@ t1 = trial_gradient(test_img, [14.0, 18.0, 7.0, 1.4, 1.1, 1.0])
 @test t1[1] < -0.01
 @test t1[2] > 0.01
 @test isapprox(t1[3],0,atol=0.01) 
-#println("empGrad: ", empGrad(test_img, [14.0, 18.0, 1.0], 1.0, Δ))
+println("empGrad: ", empGrad(test_img, [14.0, 18.0, 1.0,1.4, 1.1, 1.0], Δ))
 
 
 println()
 println("grad: ", trial_gradient(test_img, [16.0, 16.0, 7.0, 1.4, 4.0, 1.0]))
-#println("empGrad: ", empGrad(test_img, [16.0, 16.0, 4.0], 1.0, Δ))
+println("empGrad: ", empGrad(test_img, [16.0, 16.0, 7.0, 1.4, 4.0, 1.0], Δ))
 
 println()
 println("grad: ", trial_gradient(test_img, [16.0, 15.0, 7.0, 1.4, 1.1, 1.0]))
-#println("empGrad: ", empGrad(test_img, [16.0, 15.0, 2.0], 1.0, Δ))
+println("empGrad: ", empGrad(test_img, [16.0, 15.0, 7.0, 1.4, 1.1, 1.0], Δ))
 
 println()
 println("grad: ", trial_gradient(test_img, [15.0, 16.0, 7.0, 1.4, 1.1, 1.0]))
-#println("empGrad: ", empGrad(test_img, [15.0, 16.0, 2.0], 1.0, Δ))
+println("empGrad: ", empGrad(test_img, [15.0, 16.0, 7.0, 1.4, 1.1, 1.0], Δ))
 
 
 println()
 println("grad: ", trial_gradient(test_img, [16.0, 16.0, 7.0, 1.4, 1.1, 1.0]))
-#println("empGrad: ", empGrad(test_img, [16.0, 16.0, 2.0], 1.0, Δ))
+println("empGrad: ", empGrad(test_img, [16.0, 16.0, 7.0, 1.4, 1.1, 1.0], Δ))
